c2f20022b03233a4e32f82c88ed0aef0a71996bf
Andrey-Vyshegorodskiy <93198379+Andrey-Vyshegorodskiy@users.noreply.github.com>
2022-06-16 18:33:27 +0300

    redoc 10

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -3,9 +3,10 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api/docs/', TemplateView.as_view(template_name='redoc.html'),
+         name='redoc'),
     path('api/', include('api.urls')),
     path('api/', include('users.urls')),
     path('admin/', admin.site.urls),
-    path('redoc/', TemplateView.as_view(template_name='redoc.html'),
-         name='redoc'),
+
 ]
