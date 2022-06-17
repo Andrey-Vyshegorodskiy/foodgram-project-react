b214facd648d1518f83dd7537b4ce1c945467b0a
Andrey-Vyshegorodskiy <93198379+Andrey-Vyshegorodskiy@users.noreply.github.com>
2022-06-17 16:38:14 +0300

    redoc 100500

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,9 +1,6 @@
-#  from django.contrib.auth import get_user_model
 from django_filters import rest_framework as filters
 from recipes.models import Ingredient, Recipe
 from users.models import User
-
-#  User = get_user_model()
 
 
 class IngredientFilter(filters.FilterSet):
@@ -17,10 +14,9 @@ class IngredientFilter(filters.FilterSet):
 class RecipeFilter(filters.FilterSet):
     author = filters.ModelChoiceFilter(queryset=User.objects.all())
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart'
-    )
+        method='get_is_in_shopping_cart')
+    is_favorited = filters.BooleanFilter(method='get_is_favorited')
 
     def get_is_favorited(self, queryset, name, value):
         if self.request.user.is_authenticated and value:
